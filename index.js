c6804bc5c2fb0f450b33ec14b68b3323f440c256
Joost De Cock <joost@decock.org>
2019-10-12 17:43:31 +0200

    :sparkles: Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var remark = require('remark')
+var html = require('remark-html')
+var plugin = require('remark-jargon')
+var jargon = require('./jargon.js')
+
+remark()
+  .use(html)
+  .use(plugin, { jargon: jargon })
+  .process('This is a plugin for _remark_ originally written for _freesewing_.', function (err, file) {
+    console.log(String(file))
+  })
